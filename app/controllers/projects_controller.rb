18f69e72ec8e2a81eeed124c070b866a533ad46f
Nick DelRossi <nickwtf@gmail.com>
2014-12-19 07:51:29 -0500

    Create json api for Project controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,11 +1,37 @@
 class ProjectsController < ApplicationController
   helper_method :projects, :project
+  respond_to :json, only: [:index, :create, :update, :destroy]
+  respond_to :html, only: [:index]
+
+  def index
+    respond_with projects
+  end
+
+  def create
+    project = Project.create(project_params)
+    respond_with project
+  end
+
+  def update
+    project.update_attributes(project_params)
+    respond_with project
+  end
+
+  def destroy
+    respond_with project.destroy
+  end
+
+  private
+
+  def project_params
+    params.permit(:title, :description)
+  end
 
   def projects
     @projects ||= Project.all
   end
 
-  def show
+  def project
     @project ||= Project.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-
   root 'projects#index'
 
   resources :projects, only: [:index, :create, :update, :destroy]
+
+  get '*any' => 'projects#index'
 end
