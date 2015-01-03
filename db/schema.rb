3c1a048208a6bc1d512c08d2ccaa5c9ad2951c68
Nick DelRossi <nickwtf@gmail.com>
2015-01-03 07:08:48 -0500

    Remove 'description' column from project model in Rails

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150102121141) do
+ActiveRecord::Schema.define(version: 20150103120721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "projects", force: true do |t|
     t.string   "title"
-    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "author"
