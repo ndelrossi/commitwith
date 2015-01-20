4c6103345d0dc0bb83e2ba5eb69a9a8f969db5d5
Nick DelRossi <nickwtf@gmail.com>
2015-01-20 13:34:32 -0500

    Add support file for setting capybara to use webkit. Also tell Rails helper to load spec/support files

--- spec/features/visitor_visits_home_page_spec.rb
+++ spec/features/visitor_visits_home_page_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 feature "Visitor visits home page" do
-  scenario "vistor visits home page and sees website info" do
+  scenario "vistor visits home page and sees website info", js: true do
     visit root_path
 
     expect(page).to have_content("commitwith")
