50867f0ffe15a97c67cc61aac945056f4fb0f8cc
Nick DelRossi <nickwtf@gmail.com>
2015-01-21 06:40:25 -0500

    Remove test folder. Using spec

--- spec/features/visitor_visits_home_page_spec.rb
+++ spec/features/visitor_visits_home_page_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 feature "Visitor visits home page" do
-  scenario "vistor visits home page and sees website info", js: true do
+  scenario "vistor visits home page and sees title", js: true do
     visit root_path
 
-    expect(page).to have_content("commitwith")
+    expect(page).to have_content("Commitwith")
   end
 end
