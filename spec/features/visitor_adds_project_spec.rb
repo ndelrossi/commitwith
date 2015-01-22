ad57484f6e9a9a2d7841850349244cbec70e6146
Nick DelRossi <nickwtf@gmail.com>
2015-01-22 06:44:55 -0500

    Add feature test for visitor trying to add project when repo does not exist

--- spec/features/visitor_adds_project_spec.rb
+++ spec/features/visitor_adds_project_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
 
 feature "Visitor adds project" do
-  scenario "vistor adds project", js: true do
-    visit root_path
 
+  before do
+    visit root_path
+  end
+
+  scenario "vistor adds project with valid info", js: true do
     click_button "Add Project"
     fill_in "repo-name", with: "octopress"
     fill_in "repo-owner", with: "imathis"
@@ -11,5 +14,16 @@ feature "Visitor adds project" do
     click_button "modal-add-project"
 
     expect(page).to have_content("octopress")
+  end
+
+  scenario "vistor tries to add repo that does not exist", js: true do
+    click_button "Add Project"
+    fill_in "repo-name", with: "fakerepo"
+    fill_in "repo-owner", with: "fakename"
+    click_button "find-repo"
+
+    expect(page).to have_css("div.alert-danger")
+    expect(page).to have_content("Repo not found")
+    expect(page).not_to have_content("fakerepo")
   end
 end
