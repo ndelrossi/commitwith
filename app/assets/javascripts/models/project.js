56da910bdfdb91cdf4e821113f0110f5b85cce42
Nick DelRossi <nickwtf@gmail.com>
2015-01-24 10:11:43 -0500

    Update repo model code to be much more concise

--- app/assets/javascripts/models/project.js
+++ app/assets/javascripts/models/project.js
@@ -3,18 +3,18 @@ Commitwith.Models.Project = Backbone.Model.extend({
   initialize: function() {
     this.repo = new Commitwith.Models.Repo({id: "/" + this.get("author") + "/" + this.get("title")});
 
-    var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+    var self = this;
     this.repo.fetch({
-      success: __bind(function(data) {
-        return this.set({
-          language: this.repo.get("language"),
-          description: this.repo.get("description"),
-          owner: this.repo.get("owner").login,
-          open_issues_count: this.repo.get("open_issues_count"),
-          size: this.repo.get("size"),
-          last_update: this.repo.get("updated_at").split("T")[0]
+      success: function() {
+        self.set({
+          language: self.repo.get("language"),
+          description: self.repo.get("description"),
+          owner: self.repo.get("owner").login,
+          open_issues_count: self.repo.get("open_issues_count"),
+          size: self.repo.get("size"),
+          last_update: self.repo.get("updated_at").split("T")[0]
         });
-      }, this)
+      }
     });
   }
 });
