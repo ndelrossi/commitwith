a867fdce9e18f7adb2cf53eaa78a8332e1774503
Nick DelRossi <nickwtf@gmail.com>
2015-02-03 07:42:20 -0500

    Add feature specs for visitor sorting by size and open_issues

---
+++ spec/features/visitor_sorts_projects_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+feature "visitor sorts projects" do
+  let!(:project1) { create(:commitwith, open_issues: 1, size: 1000) }
+  let!(:project2) { create(:rails, open_issues: 100, size: 400) }
+  let!(:project3) { create(:glide, open_issues: 50, size: 700) }
+
+  before do
+    visit root_path
+  end
+
+  scenario "visitor sorts projects by number of open issues", js: true do
+    click_link "sort-open_issues"
+
+    expect(page.text).to match(/rails.*glide.*commitwith/)
+
+    click_link "sort-open_issues"
+
+    expect(page.text).to match(/commitwith.*glide.*rails/)
+  end
+
+  scenario "visitor sorts projects by size", js: true do
+    click_link "sort-size"
+
+    expect(page.text).to match(/commitwith.*glide.*rails/)
+
+    click_link "sort-size"
+
+    expect(page.text).to match(/rails.*glide.*commitwith/)
+  end
+end
