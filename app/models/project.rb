56fde67b6fe98ada8800b206bfe8b5f28c8df1c3
Nick DelRossi <nickwtf@gmail.com>
2015-02-17 08:42:53 -0500

    Fix bug in Project.update_info method. Was using old 'title' attribute which is now 'name'

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ActiveRecord::Base
 
   def self.update_info
     Project.order('updated_at ASC').limit(2).each do |project|
-      response = HTTParty.get("https://api.github.com/repos/#{project.author}/#{project.title}")
+      response = HTTParty.get("https://api.github.com/repos/#{project.author}/#{project.name}")
       project.update_attributes(size:        response["size"],
                                 open_issues: response["open_issues_count"],
                                 last_update: response["updated_at"])
