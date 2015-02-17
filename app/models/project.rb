41f7ad6628ddb94a034c4c3ea4e03055d87948f0
Nick DelRossi <nickwtf@gmail.com>
2015-02-17 08:19:40 -0500

    Update cronjob to not change project description

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,8 +3,7 @@ class Project < ActiveRecord::Base
   def self.update_info
     Project.order('updated_at ASC').limit(2).each do |project|
       response = HTTParty.get("https://api.github.com/repos/#{project.author}/#{project.title}")
-      project.update_attributes(description: response["description"],
-                                size:        response["size"],
+      project.update_attributes(size:        response["size"],
                                 open_issues: response["open_issues_count"],
                                 last_update: response["updated_at"])
     end
