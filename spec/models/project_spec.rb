650ddc5588bb993f8e75ee49f53001e8fc70f5bf
Nick DelRossi <nickwtf@gmail.com>
2015-03-02 07:10:31 -0500

    Add project test for active scope

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe Project, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Project do
+  
+  describe "scopes" do
+    describe "active" do
+      it "returns all projects with active set to true" do
+        create(:project, name: "Foo", email: "fake@fake.com", active: true)
+        create(:project, name: "FooBar", email: "fake@fake.com", active: false)
+        create(:project, name: "FooBaz", email: "fake@fake.com", active: true)
+
+        expect(Project.active.pluck(:name)).to eq ["Foo", "FooBaz"]
+      end
+    end
+  end
 end
